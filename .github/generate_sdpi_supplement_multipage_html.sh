80f50215b2c28aaa185dcc085c298638424fef11
David Gregorczyk <54440227+d-gregorczyk@users.noreply.github.com>
2022-09-02 17:18:41 +0200

    Update generate_sdpi_supplement_multipage_html.sh
    #49 Copy images to output

--- .github/generate_sdpi_supplement_multipage_html.sh
+++ .github/generate_sdpi_supplement_multipage_html.sh
@@ -4,3 +4,4 @@ gem install asciidoctor-diagram
 gem install asciidoctor-multipage
 asciidoctor -V
 asciidoctor -r asciidoctor-diagram -r asciidoctor-multipage -b multipage_html5 -D SDPi_Supplement/sdpi-supplement/multipage SDPi_Supplement/asciidoc/sdpi-supplement.adoc
+cp -R SDPi_Supplement/asciidoc/images SDPi_Supplement/sdpi-supplement/multipage/images
