2a222ec9ca99e9037c931f79a279510cfb668053
David Gregorczyk <54440227+d-gregorczyk@users.noreply.github.com>
2022-09-05 17:57:12 +0200

    #49 Delete temp directories after generating

--- .github/generate_sdpi_supplement_multipage_html.sh
+++ .github/generate_sdpi_supplement_multipage_html.sh
@@ -6,3 +6,5 @@ asciidoctor -V
 cd SDPi_Supplement/asciidoc
 asciidoctor -r asciidoctor-diagram -r asciidoctor-multipage -b multipage_html5 -D ../sdpi-supplement/multipage sdpi-supplement.adoc
 cp -R images ../sdpi-supplement/multipage/images
+rm -rf ../sdpi-supplement/multipage/.asciidoctor
+rm -rf ../sdpi-supplement/.asciidoctor
