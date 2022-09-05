4371059a7ae57dc22a593a596af4c2950e42e689
David Gregorczyk <54440227+d-gregorczyk@users.noreply.github.com>
2022-09-05 17:59:54 +0200

    #49 Copy images folder for single page HTML

--- .github/generate_sdpi_supplement_multipage_html.sh
+++ .github/generate_sdpi_supplement_multipage_html.sh
@@ -7,4 +7,5 @@ cd SDPi_Supplement/asciidoc
 asciidoctor -r asciidoctor-diagram -r asciidoctor-multipage -b multipage_html5 -D ../sdpi-supplement/multipage sdpi-supplement.adoc
 cp -R images ../sdpi-supplement/multipage/images
 rm -rf ../sdpi-supplement/multipage/.asciidoctor
+cp -R images ../sdpi-supplement/images
 rm -rf ../sdpi-supplement/.asciidoctor
