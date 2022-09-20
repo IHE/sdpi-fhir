cad48363f0999082885ee6c6c54d5799e954075b
David Gregorczyk <54440227+d-gregorczyk@users.noreply.github.com>
2022-09-20 22:27:22 +0200

    Adapt to generate images beforehand

--- .github/copy_images.sh
+++ .github/copy_images.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+gem install asciidoctor
+gem install asciidoctor-diagram
+gem install asciidoctor-multipage
+asciidoctor -V
 cd SDPi_Supplement/asciidoc
+asciidoctor -r asciidoctor-diagram -r asciidoctor-multipage -b multipage_html5 -D ../ sdpi-supplement.adoc
 cp -R images ../sdpi-supplement/images
 rm -rf ../sdpi-supplement/.asciidoctor
