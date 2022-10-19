ec8bf9e8306a3965b2f3cd4c9ab9eb28243fb0cf
David Gregorczyk <david.gregorczyk@draeger.com>
2022-10-19 13:39:39 +0200

    #103 Add verification of requirement level attribute existence. Change requirement block name to sdpi_requirement.

--- SDPi_Supplement/asciidoc-converter/src/main/kotlin/org/sdpi/asciidoc/model/SdpiRequirement.kt
+++ SDPi_Supplement/asciidoc-converter/src/main/kotlin/org/sdpi/asciidoc/model/SdpiRequirement.kt
@@ -1,13 +1,32 @@
 package org.sdpi.asciidoc.model
 
-import org.sdpi.asciidoc.blockId
-import org.sdpi.asciidoc.blockTitle
+import org.sdpi.asciidoc.*
 
+/**
+ * Definition of requirement levels.
+ *
+ * @property keyword The keyword as it is supposed to appear as a value to the attribute key.
+ */
+enum class RequirementLevel(val keyword: String) {
+    MAY("may"),
+    SHOULD("should"),
+    SHALL("shall")
+}
+
+/**
+ * Structure that represents an SDPi requirement.
+ *
+ * @property number The requirement number as an integer (no leading R or zero-padding).
+ * @property level The requirement level as specified by the attribute [BlockAttribute.REQUIREMENT_LEVEL].
+ * @property asciiDocAttributes All attributes captured by the block that represents this requirement.
+ * @property asciiDocLines The actual ASCIIdoc source.
+ */
 data class SdpiRequirement(
     val number: Int,
-    val asciiDocAttributes: Map<String, Any>,
+    val level: RequirementLevel,
+    val asciiDocAttributes: Attributes,
     val asciiDocLines: List<String>
 ) {
-    val blockId = blockId(asciiDocAttributes)
-    val blockTitle = blockTitle(asciiDocAttributes)
+    val blockId = asciiDocAttributes.id()
+    val blockTitle = asciiDocAttributes.title()
 }
