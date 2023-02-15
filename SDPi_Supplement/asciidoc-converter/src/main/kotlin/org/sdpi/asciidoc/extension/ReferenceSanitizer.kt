d27c8f3db79aeb5226af2497081f8f21384bd1c9
David Gregorczyk <david.gregorczyk@web.de>
2023-02-15 13:06:21 +0100

    Fixes for the numbering.

--- SDPi_Supplement/asciidoc-converter/src/main/kotlin/org/sdpi/asciidoc/extension/ReferenceSanitizer.kt
+++ SDPi_Supplement/asciidoc-converter/src/main/kotlin/org/sdpi/asciidoc/extension/ReferenceSanitizer.kt
@@ -20,6 +20,9 @@ data class LabelInfo(
 
 class ReferenceSanitizer(private val anchorLabels: Map<String, LabelInfo>) : Postprocessor() {
     override fun process(document: Document, output: String): String {
+        val sectionPrefix = prefixFor(OPTION_SECTION_PREFIX, document.attributes, "Section")
+        val appendixPrefix = prefixFor(OPTION_APPENDIX_PREFIX, document.attributes, "Appendix")
+
         val doc = Jsoup.parse(output, "UTF-8")
 
         val anchors = doc.getElementsByTag("a")
@@ -28,9 +31,9 @@ class ReferenceSanitizer(private val anchorLabels: Map<String, LabelInfo>) : Pos
                 val href = anchor.attr("href") ?: ""
                 anchorLabels[URI.create(href).fragment]?.let {
                     when (it.source) {
-                        LabelSource.SECTION -> anchor.text("Section ${it.label}")
+                        LabelSource.SECTION -> anchor.text("$sectionPrefix${it.label}")
                         LabelSource.TABLE_OR_FIGURE -> anchor.text(it.label)
-                        LabelSource.APPENDIX -> anchor.text("Appendix ${it.prefix}:${it.label}")
+                        LabelSource.APPENDIX -> anchor.text("$appendixPrefix${it.prefix}:${it.label}")
                     }
                 }
             }
@@ -39,5 +42,19 @@ class ReferenceSanitizer(private val anchorLabels: Map<String, LabelInfo>) : Pos
         return doc.html()
     }
 
+    private fun prefixFor(option: String, attributes: Map<String, Any>, defaultValue: String) =
+        attributes[option]?.toString()?.let {
+            if (it.isBlank()) {
+                ""
+            } else {
+                "${it.trim()} "
+            }
+        } ?: "$defaultValue "
+
     private fun isInToc(element: Element) = element.parents().firstOrNull { it.id() == "toc" } != null
+
+    private companion object {
+        const val OPTION_SECTION_PREFIX = "sdpi_ref_section_prefix"
+        const val OPTION_APPENDIX_PREFIX = "sdpi_ref_appendix_prefix"
+    }
 }
