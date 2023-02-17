de5edfd096edf98ba950a8c04d5160349708c1bc
David Gregorczyk <54440227+d-gregorczyk@users.noreply.github.com>
2023-02-17 13:39:21 +0100

    Update copy_images.sh

--- .github/copy_images.sh
+++ .github/copy_images.sh
@@ -7,5 +7,5 @@ cd SDPi_Supplement/asciidoc
 asciidoctor -r asciidoctor-diagram -r asciidoctor-multipage -b multipage_html5 -D ../ sdpi-supplement.adoc
 cp -R images ../sdpi-supplement/images
 rm -rf ../sdpi-supplement/.asciidoctor
-rm -rf ../.asciidoctor
+find ../ -name '.asciidoctor' -exec rm -rf {} \; 
 cp -R js ../sdpi-supplement/js
