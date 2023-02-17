5f40e9f6addb8e1c7cba7d8aa4a50bc8706d6e62
David Gregorczyk <54440227+d-gregorczyk@users.noreply.github.com>
2023-02-17 16:09:32 +0100

    Actions update (#404)
    * Remove multipage from artifact
    
    * Fix artifact branch naming
    
    * Update copy_images.sh
    
    * Update copy_images.sh
    
    * Update copy_images.sh
    
    * Change actions script task order
    
    * Update copy_images.sh

--- .github/copy_images.sh
+++ .github/copy_images.sh
@@ -6,6 +6,6 @@ asciidoctor -V
 cd SDPi_Supplement/asciidoc
 asciidoctor -r asciidoctor-diagram -r asciidoctor-multipage -b multipage_html5 -D ../ sdpi-supplement.adoc
 cp -R images ../sdpi-supplement/images
-rm -rf ../sdpi-supplement/.asciidoctor
-rm -rf ../.asciidoctor
 cp -R js ../sdpi-supplement/js
+rm -rf ../sdpi-supplement/.asciidoctor
+rm -rf ../sdpi-supplement/readme.md
